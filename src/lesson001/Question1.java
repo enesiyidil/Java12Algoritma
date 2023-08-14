8e3d2d37acd21e75715c153630ec5b73320481c9
enes iyidil <enesiyidil16@gmail.com>
2023-08-14 14:15:52 +0300

    ProjeninOlusturulmasi

---
+++ src/lesson001/Question1.java
@@ -0,0 +1,17 @@
+package lesson001;
+
+public class Question1 {
+	/*
+	 * Class(Sınıf) ilimleri PascalCase tarzında isimlendirilir.
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * 
+		 */
+		
+		System.out.println("Github Denemesi");
+
+	}
+
+}
