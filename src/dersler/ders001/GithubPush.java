af76028aea99f6075fb5c1137e73315fc04ec985
enes iyidil <enesiyidil16@gmail.com>
2023-08-14 14:37:47 +0300

    ProjeninIsimlendirmeGuncellemesi

---
+++ src/dersler/ders001/GithubPush.java
@@ -0,0 +1,46 @@
+/*
+ * BilgeAdamBoost Java Full-Stack eğitimi
+ */
+package dersler.ders001;
+
+/**
+ * GithubPush Class'ı projenin Github üzerine yüklenmesini sağlayan sınıftır.
+ * 
+ * @author enesb
+ */
+public class GithubPush {
+	/*
+	 * Class(Sınıf) ilimleri PascalCase tarzında isimlendirilir.
+	 */
+	
+	/**
+	 * GithubPush sınıfının main fonksiyonudur.
+	 * 
+	 * @author enesb
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * create a new repository on the command line
+		 * 
+		 * echo "# Java12Algoritma" >> README.md
+		 * git init
+		 * git add README.md
+		 * git commit -m "first commit"
+		 * git branch -M main
+		 * git remote add origin https://github.com/enesiyidil/Java12Algoritma.git
+		 * git push -u origin main
+		 */
+		
+		/*
+		 * push an existing repository from the command line
+		 * 
+		 * git remote add origin https://github.com/enesiyidil/Java12Algoritma.git
+		 * git branch -M main
+		 * git push -u origin main
+		 */
+		System.out.println("Github Denemesi");
+
+	}
+}
