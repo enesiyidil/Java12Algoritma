f4f3f79d7eb4fcfee77420dde3b1343071e760ea
enes iyidil <enesiyidil16@gmail.com>
2023-08-14 16:36:40 +0300

    Ders001 sonu

---
+++ src/dersler/ders001/Problem3.java
@@ -0,0 +1,29 @@
+/*
+ * BilgeAdamBoost Java Full-Stack eğitimi
+ */
+package dersler.ders001;
+
+import java.util.Scanner;
+
+/**
+ * Verilen ürün satış fiyatının ham fiyatını bulunuz.
+ * Satış fiyatı %18 kdv ve %15 kar oranlarını içermektedir.
+ */
+public class Problem3 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		double vergiOrani = 1.18, karOrani = 1.15;
+		int fiyat;
+		double hamFiyat;
+		
+		System.out.println("Ürün fiyatını giriniz.");
+		fiyat = scanner.nextInt();
+		
+		hamFiyat = fiyat / (vergiOrani * karOrani);
+		System.out.println("Verilen ürün fiyatının ham fiyatı = " + hamFiyat);
+
+	}
+
+}
