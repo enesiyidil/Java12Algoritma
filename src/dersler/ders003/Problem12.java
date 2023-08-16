05265c26e3f96c67fede6ab456866d65e1bae468
enes iyidil <enesiyidil16@gmail.com>
2023-08-16 16:49:42 +0300

    Ders 3

---
+++ src/dersler/ders003/Problem12.java
@@ -0,0 +1,31 @@
+/*
+ * BilgeAdamBoost Java Full-Stack eğitimi
+ */
+package dersler.ders003;
+
+import java.util.Scanner;
+
+/**
+ * Girilen iki sayının toplamının tek mi çift mi olma durumuna göre true veya false veren program
+ */
+public class Problem12 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("1. Sayıyı giriniz.");
+		int userNum = scanner.nextInt(), toplam = userNum;
+		System.out.println("2. Sayıyı giriniz.");
+		userNum = scanner.nextInt();
+		toplam += userNum;
+		
+		if(toplam%2 == 0) {
+			System.out.println(true);
+		}else {
+			System.out.println(false);
+		}
+
+	}
+
+}
---
+++ src/dersler/ders003/Problem17.java
@@ -0,0 +1,28 @@
+/*
+ * BilgeAdamBoost Java Full-Stack eğitimi
+ */
+package dersler.ders003;
+
+import java.util.Scanner;
+
+/**
+ * Girilen sayının basamakları toplamını veren program
+ */
+public class Problem17 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Sayı giriniz");
+		int sayi = scanner.nextInt(), toplam = 0;
+		
+		while(sayi != 0) {
+			toplam += sayi % 10;
+			sayi /= 10;
+		}
+		System.out.println("Girilen sayının basamakları toplamı : " + toplam);
+
+	}
+
+}
